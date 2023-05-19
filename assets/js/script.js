ec23e98d33f4daeab3ee6813ee42917e6a0c9354
Milea90 <>
2023-05-19 13:34:09 +0000

    add aria labels to forms

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,13 @@ function is_checked()
     const blue_checked = document.getElementById("color-blue").checked;
     const green_checked = document.getElementById("color-green").checked;
     const red_checked = document.getElementById("color-red").checked;
-
+   
     if(blue_checked === false && green_checked === false && red_checked === false)
     {alert("Oops... You did not select an option!");
      return false;}
      
-     else {return true;} 
+     else {return true;}
+
 }
 
 function contact_form() {
