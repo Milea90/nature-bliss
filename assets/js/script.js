c2312d37545c8e2649b0b99955d8f4d43cd27d56
Milea90 <>
2023-09-08 08:40:39 +0000

    add javascript to the wheel

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,11 @@ function contact_form() {
         return false;}
 
     else {return true;}}
+
+let container = document.querySelector(".wheel");
+let btn = document.getElementById("spin");
+let number = Math.ceil(Math.random() * 1000);
+btn.onclick = function () {
+    container.style.transform = "rotate(" + number + "deg)";
+    number += Math.ceil(Math.random() * 1000);
+};
